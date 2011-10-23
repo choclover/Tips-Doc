907e6bfb4c8c53d89a69337b59e71e5346220387
Simon He <hemi12@tom.com>
2011-10-23 22:18:21 +0800

    oct 23 commit

--- src/com/studentpaldaemon/app/DaemonService.java
+++ src/com/studentpaldaemon/app/DaemonService.java
@@ -1,6 +1,16 @@
 package com.studentpaldaemon.app;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.studentpal.engine.Event.*;
+
+import com.studentpal.app.MainAppService;
+import com.studentpal.app.ResourceManager;
+import com.studentpal.app.handler.DaemonHandler;
+import com.studentpal.engine.Event;
+import com.studentpaldaemon.util.Logger;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -14,92 +24,293 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 
 public class DaemonService extends Service {
   private static final String TAG = "@@ DaemonService";
+  private static final boolean forTest = true;
   
-  ActivityManager activityManager = null;
-  
+  /* 
+   * Field members
+   */
+  private ActivityManager   activityManager = null;
+  private Timer             _watchdogTimer = null;
+  private TimerTask         _watchdogTask  = null;
+
   /**
    * Target we publish for clients to send messages to IncomingHandler.
    */
-  final Messenger mMessenger = new Messenger(new IncomingHandler());
-
+  final IncomingHandler msgHandler = new IncomingHandler();
+  final Messenger mMsgerToMyself = new Messenger(msgHandler );
+  /** Messenger for communicating with MainApp service. */
+  private Messenger mMsgerToMainApp = null;
   
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate() {
-        super.onCreate();
-//        setContentView(R.layout.main);
-//        
-        activityManager = (ActivityManager)this.getSystemService(Context.ACTIVITY_SERVICE);
-        
-        Thread t = new Thread(new Runnable() {
-          @Override
-          public void run() {
-            int i = 1;
-            while (true) {
-              android.util.Log.v(TAG, "I am running @ " + i++);
-              
-              String procName = "com.studentpal";
-              if (findRunningAppProcess(procName) == null) {
-                android.util.Log.d(TAG, procName + " is NOT running!!");
-                
-                Intent intent = new Intent();
-                ComponentName comp = new ComponentName("com.studentpal",
-                    "com.studentpal.ui.LaunchScreen");
-                intent.setComponent(comp);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                Bundle bundle = new Bundle();
-                bundle.putBoolean("show_launcher_ui", false);
-                intent.putExtras(bundle);
-                
-                startActivity(intent);
-              }
-              
-              try {
-                Thread.sleep(500);
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
-            }
-          }
-        });
-        t.start();
+  //For test
+  //private boolean stopCounterThd = false;
+  
+  /** 
+   * Called when the activity is first created. 
+   */
+  @Override
+  public void onCreate() {
+    Logger.d(TAG, "onCreate!");
+    super.onCreate(); 
+  } 
+  
+  //This is the old onStart method that will be called on the pre-2.0
+  // platform. On 2.0 or later we override onStartCommand() so this
+  // method will not be called.
+  @Override
+  public void onStart(Intent intent, int startId) {
+    Logger.d(TAG, "onStart!");
+    onStartCommand(intent, 0, startId);
+  }
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    Logger.d(TAG, "onStartCommand!");
+    
+    // No intent, tell the system not to restart us.
+    if (intent == null) {
+      stopSelf();
+      return START_NOT_STICKY;
     }
     
-    private RunningAppProcessInfo findRunningAppProcess(String classname) {
-      RunningAppProcessInfo result = null;
+    if (forTest) {
+      runConterThread();
+    }
+    
+    // We want this service to continue running until it is explicitly
+    // stopped, so return sticky.
+    return START_STICKY;
+  }
+
+  @Override
+  public IBinder onBind(Intent arg0) {
+    Logger.d(TAG, "onBind!");
+    return mMsgerToMyself.getBinder();
+  }
+  
+  public void onRebind (Intent arg0) {
+    Logger.d(TAG, "onRebind!");
+  }
+  
+  public boolean onUnind(Intent arg0) {
+    Logger.d(TAG, "onUnind!");
+    return true;
+  }
+  
+  @Override
+  public void onDestroy() {
+    Logger.d(TAG, "onDestroy(), stopping myself!");
+    
+    stopConterThread();
+    
+    runWatchdogTask(false);
+    mMsgerToMainApp = null;
+    stopSelf();
+    
+    exitApp();
+  }
+
+  // ///////////////////////////////////////////////////////////////////////////
+  static class CounterThread {
+    private static boolean stop = false;
+    private static Thread  inner_th = null;
+
+    public static void stop() {
+      Logger.d(TAG, "CounterThread.stop()");
+      stop = true;
+      if (inner_th != null) {
+        inner_th.interrupt();
+        inner_th = null;
+      }
+    }
+
+    public static void start() {
+      Logger.d(TAG, "CounterThread.start()");
+      if (inner_th != null) return;
       
-      List<RunningAppProcessInfo> processes = activityManager
-          .getRunningAppProcesses();
+      stop = false;
+      inner_th = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          int cnt = 1;
+          while (false == stop) {
+            Logger.v(TAG, "Daemon is running @ " + cnt++);
+
+            try { Thread.sleep(3000); }
+            catch (InterruptedException e) { }
+          }
+          Logger.v(TAG, "Counter thread " + this + " is stopped!");
+        }
+      });
+      inner_th.start();
+    }
+  }
+  
+  private void runConterThread() {
+    if (forTest) CounterThread.start();
+  }
+  private void stopConterThread() {
+    if (forTest) CounterThread.stop();
+  }
+  
+  private TimerTask getWatchdogTask() {
+    TimerTask task = new TimerTask() {
+      @Override
+      public void run() {
+        //Logger.v(TAG, "Daemon watchdog task starts to run!");
+        sendWatchdogMsg();
+      }
+    };
+    return task;
+  }
+  
+  private void sendWatchdogMsg() {
+    if (mMsgerToMainApp == null) {
+      Logger.w(TAG, "mMsgerToMainApp should NOT be NULL!");
+      return;
+    }
+    
+    try {
+      Message wdMsg = Message.obtain(null, SIGNAL_TYPE_DAEMON_WD_REQ);
+      this.mMsgerToMainApp.send(wdMsg);
+      this.msgHandler.sendEmptyMessageDelayed(SIGNAL_TYPE_DAEMON_WD_TIMEOUT, 
+          DaemonHandler.DAEMON_WATCHDOG_TIMEOUT);
+    } catch (RemoteException e) {
+      Logger.w(TAG, "sendWatchdogReq() got exception with "+e.toString());      
+      // The client is dead.
+      runWatchdogTask(false);
+      reLaunchClientApp();
+    }
+  }
+  
+  public void runWatchdogTask(boolean runWD) {
+    Logger.d(TAG, "Ready to run watchdog is: "+runWD);
+    
+    if (runWD==true) {
+      //we cannot reuse the old Timer if it is ever cancelled, so have to recreate one
+      _watchdogTimer = new Timer();
+      if (_watchdogTask == null) {
+        _watchdogTask = getWatchdogTask();
+      }
+      _watchdogTimer.schedule(_watchdogTask, 0, DaemonHandler.DAEMON_WATCHDOG_INTERVAL);
+      
+    } else {
+      msgHandler.removeMessages(SIGNAL_TYPE_DAEMON_WD_TIMEOUT);
+      
+      if (_watchdogTask != null) {
+        _watchdogTask.cancel();
+        _watchdogTask = null;
+      }
+      if (_watchdogTimer != null) {
+        _watchdogTimer.purge();
+        _watchdogTimer.cancel();
+        _watchdogTimer = null;
+      }
+    }
+  }
+  
+  private void handleClientTimeout() {
+    Logger.d(TAG, "handleClientTimeout()");
+    
+    msgHandler.removeMessages(SIGNAL_TYPE_DAEMON_WD_TIMEOUT);
+    reLaunchClientApp();
+  }
+  
+  private boolean reLaunchClientApp() {
+    boolean result = true;
+    
+    String procName = ResourceManager.APPLICATION_PKG_NAME;
+    if (findRunningAppProcess(procName) == null) {
+      Logger.w(TAG, procName + " is NOT running, relaunching it!");
+
+      Intent intent = new Intent();
+//      Bundle bundle = new Bundle();
+//      bundle.putBoolean(CFG_SHOW_LAUNCHER_UI, false);
+//      intent.putExtras(bundle);
+      ComponentName comp = new ComponentName(procName,
+          MainAppService.NAME);
+      intent.setComponent(comp);
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      intent.putExtra(Event.TAGNAME_BUNDLE_PARAM, MainAppService.CMD_START_WATCHING_APP);
+      
+      startService(intent);  //hemerr
+      
+    } else {
+      Logger.d(TAG, procName + " is still running!!");
+    }
+    return result;
+  }
+  
+  /**
+   * Utility Functions
+   */
+  private RunningAppProcessInfo findRunningAppProcess(String appProcName) {
+    RunningAppProcessInfo result = null;
+
+    if (activityManager == null) {
+      activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+    }
+    
+    List<RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
+    if (processes != null) {
       for (RunningAppProcessInfo process : processes) {
         String pname = process.processName;
-        //android.util.Log.d(TAG, pname);
-        if (classname.equals(pname)) {
+        // Logger.d(TAG, pname);
+        if (appProcName.equals(pname)) {
           result = process;
           break;
         }
       }
-      return result;
     }
-
+    return result;
+  }
+  
+  private void exitApp() {
+    int pid = android.os.Process.myPid();
+    android.os.Process.killProcess(pid);
+    //System.exit(1);
+  }
+  
+  // ///////////////////////////////////////////////////////////////////////////
+  /**
+   * Handler of incoming messages from clients.
+   */
+  class IncomingHandler extends Handler {
     @Override
-    public IBinder onBind(Intent arg0) {
-      return mMessenger.getBinder();
-
+    public void handleMessage(Message msg) {
+      int sigType = msg.what;
+      //Logger.d(TAG, "Signal type is: "+sigType);
+      switch(sigType) {
+        case SIGNAL_TYPE_START_DAEMONTASK:
+          mMsgerToMainApp = msg.replyTo;
+          runWatchdogTask(true);
+          break;
+          
+        case SIGNAL_TYPE_STOP_DAEMONTASK:
+          runWatchdogTask(false);
+          stopConterThread();
+          break;
+        
+        case SIGNAL_TYPE_EXIT_DAEMONTASK:
+          onDestroy();
+          break;
+       
+        case SIGNAL_TYPE_DAEMON_WD_RESP:
+          this.removeMessages(SIGNAL_TYPE_DAEMON_WD_TIMEOUT);
+          break; 
+          
+        case SIGNAL_TYPE_DAEMON_WD_TIMEOUT:
+          runWatchdogTask(false);
+          handleClientTimeout();
+          break;
+          
+        default:
+          super.handleMessage(msg);
+        
+      }
     }
-    
-    
-    
-    ////////////////////////////////////////////////////////////////////////////
-    /**
-     * Handler of incoming messages from clients.
-     */
-    class IncomingHandler extends Handler {
-        @Override
-        public void handleMessage(Message msg) {
-        }
-    }
+  }
 
 }
