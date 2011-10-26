87ae2b35d115a0bfa2ad84fbb24444437120eae2
Simon He <Simon.Ming@tekelec.com>
2011-10-26 17:45:40 +0800

    oct 26 commit

--- runWsCli.sh
+++ runWsCli.sh
@@ -6,8 +6,13 @@ CP_VAR=./bin:./build/classes:/media/Tools/apache-mina-2.0.3/dist/mina-core-2.0.3
 # $CP_VAR=$CP_VAR:/media/Tools/apache-cxf-2.4.2/lib/neethi-3.0.0.jar:/media/Tools/apache-cxf-2.4.2/lib/xmlschema-core-2.0.jar:/media/Tools/apache-cxf-2.4.2/lib/wsdl4j-1.6.2.jar:/media/Tools/apache-cxf-2.4.2/lib/jaxb-xjc-2.2.1.1.jar:/media/Tools/apache-cxf-2.4.2/lib/jaxb-impl-2.2.1.1.jar:/media/Tools/apache-cxf-2.4.2/lib/cxf-2.4.2.jar:
 
 echo "Classpath is:"
-echo $CP_VAR
+#echo $CP_VAR
 
-java -cp $CP_VAR      studentpal.test.client.WsClientCli  %1 %2 %3
+CMD="java -cp $CP_VAR       studentpal.test.client.WsClientCli  %1 %2 %3 "
+CMD="java -cp $CP_VAR       studentpal.test.client.WsClientCli -catexml ./res/CateDef.xml  %1 %2 %3 "
+
+echo $CMD
+
+$CMD
 
 
