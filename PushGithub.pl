ab43e45c17bc6fa40c858111a055ba3f41203081
Simon He <Simon.Ming@tekelec.com>
2012-01-06 09:07:26 +0800

    2012-01-06 commit

--- PushGithub.pl
+++ PushGithub.pl
@@ -54,6 +54,32 @@ my %projReposMap_lnx = (
   "SpalSvr"                => "git\@bitbucket.org:choclover/studentpalsvr.git",
 );
 
+if (0) {
+  my %projReposMap_common = (
+  "SpalAdmin"              => "git\@bitbucket.org:choclover/studentpaladmin.git",
+  "MyPkgInstaller"         => "git\@bitbucket.org:choclover/mypkginstaller_froyo.git",
+  #"MyPkgInstaller"         => "git\@github.com:choclover/CustomPkgInstaller.git",
+  "Tips_Doc"               => "git\@github.com:choclover/Tips-Doc.git",
+  "SysPkgInstaller"        => "git\@bitbucket.org:choclover/syspkginstaller_froyo",
+  );
+
+my %projReposMap_win = (
+  #1
+  "StudentPalClient"       => "git\@bitbucket.org:choclover/studentpalclient.git",
+  #2
+  "StudentPalClientDeamon" => "git\@bitbucket.org:choclover/studentpalclientdaemon.git",
+  #3
+  "SpalSvr"                => "git\@bitbucket.org:choclover/studentpalsvr.git",
+);
+
+my %projReposMap_lnx = (
+  "SpalClient"             => "git\@bitbucket.org:choclover/studentpalclient.git",
+  "SpalClientDaemon"       => "git\@bitbucket.org:choclover/studentpalclientdaemon.git",
+  "SpalSvr"                => "git\@bitbucket.org:choclover/studentpalsvr.git",
+);
+  
+}
+
 #*****************************AUXILIARY  FUNCTIONS****************************#
 sub DEBUG_INFO {
   return if (!$bDEBUG);
@@ -207,7 +233,7 @@ sub pull_repos {
 
   foreach my $dire (sort keys %$refProjReposMap) {
   	my $gitUrl = $$refProjReposMap{$dire};
-  	my $path = "$gRootDir/$dire";  P("\n$path\n");
+  	my $path = "$gRootDir/$dire";  P("\n@@ cd $path\n");
 
     my $cdDir = "";
     my $cmdStr = "";
@@ -242,7 +268,7 @@ sub push_repos {
 
   foreach my $dire (sort keys %$refProjReposMap) {
   	my $gitUrl = $$refProjReposMap{$dire};
-  	my $path = "$gRootDir/$dire";  P("\n$path\n");
+  	my $path = "$gRootDir/$dire";  P("\n@@ cd $path\n");
 
     my $cdDir = "cd $path; ";
     my $cmdStr = "";
@@ -271,12 +297,12 @@ sub status_repos {
 
   foreach my $dire (sort keys %$refProjReposMap) {
   	my $gitUrl = $$refProjReposMap{$dire};
-  	my $path = "$gRootDir/$dire";  P("\n$path\n");
+  	my $path = "$gRootDir/$dire";  P("\n@@ cd $path\n");
 
     my $cdDir = "cd $path; ";
     my $cmdStr = "";
 
-    $cmdStr = $cdDir . "git status ";
+    $cmdStr = $cdDir . "git status; git remote -v ";
     runSysCmd($cmdStr);
   }
 }
